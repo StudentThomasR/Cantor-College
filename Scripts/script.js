328a48e1acb095fb6f979090dbcea2868c9d91b2
thomasrileydev <52782404+thomasrileydev@users.noreply.github.com>
2021-12-20 13:49:03 +0000

    commit

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-    var autoplaySlider = $('#lightSlider').lightSlider({
-        auto: true,
-        loop: true,
-        pauseOnHover: true,
-        onBeforeSlide: function (el) {
-        
-        }
-    });
+    
 $("#Burger").click(function (){
 $("nav").slideToggle("fast")
 })
---
+++ Scripts/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+var autoplaySlider = $('#lightSlider').lightSlider({
+        auto: true,
+        loop: true,
+        pauseOnHover: true,
+        onBeforeSlide: function (el) {
+        
+        }
+    });
+    });
